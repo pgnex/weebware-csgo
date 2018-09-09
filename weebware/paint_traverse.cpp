51a9b6263a824a7f62ce47c4d54fc74c017f6dd1
Kite <jackywen4@gmail.com>
2018-09-09 23:39:25 +1000

    fps stuff

--- weebware/paint_traverse.cpp
+++ weebware/paint_traverse.cpp
@@ -15,11 +15,6 @@ void hook_functions::paint_traverse(unsigned int v, bool f, bool a)
 
 	auto ilocal = g_weebware.g_entlist->getcliententity(g_weebware.g_engine->get_local()); // getting localplayer
 
-	if (!ilocal || ilocal->m_bGunGameImmunity() || !ilocal->is_valid_player() || !g_weebware.g_engine->is_connected() || !g_weebware.g_engine->is_in_game() || ilocal->m_fFlags() & fl_frozen) {
-		g_Walkbot.m_target_area = nullptr;
-		g_Walkbot.m_TargetEntity = nullptr;
-	}
-
 	if (!ilocal || !g_weebware.g_engine->is_connected() || !g_weebware.g_engine->is_in_game()) {
 		g_dme.night_mode_update = true;
 	}
