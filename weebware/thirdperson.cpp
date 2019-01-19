149b53a1e49cfc899aefceee5c5d369adc714eb1
nexzoz <nexzozgaming@gmail.com>
2019-01-18 22:16:56 -0800

    not rly but i tried
    not rly but i tried

--- weebware/thirdperson.cpp
+++ weebware/thirdperson.cpp
@@ -2,25 +2,21 @@
 #include "thirdperson.h"
 
 static bool done = false;
+
 void thirdperson::fsn(c_base_entity* local) {
-	c_convar* tp = g_weebware.g_convars->find_cvar("thirdperson");
-	c_convar* fp = g_weebware.g_convars->find_cvar("firstperson");
 
-	if (GetAsyncKeyState(g_weebwarecfg.thirdperson)) {
-
+	if (GetAsyncKeyState(g_weebwarecfg.thirdperson) && !done) {
+		if (local && local->is_valid_player()) {
+			c_convar* tp = g_weebware.g_convars->find_cvar("thirdperson");
+			tp->SetValue("1");
+			done = true;
+		}
 	}
-	//if (!done && g_weebwarecfg.thirdperson) {
-	//	if (local && local->is_valid_player()) {
-	//		c_convar* tp = g_weebware.g_convars->find_cvar("thirdperson");
-	//		tp->SetValue("1");
-	//		done = true;
-	//	}
-	//}
-	//else if (!g_weebwarecfg.thirdperson && done) {
-	//	if (local && local->is_valid_player()) {
-	//		c_convar* fp = g_weebware.g_convars->find_cvar("firstperson");
-	//		fp->SetValue("1");
-	//		done = false;
-	//	}
-	//}
+	else if (GetAsyncKeyState(g_weebwarecfg.thirdperson) && done) {
+		if (local && local->is_valid_player()) {
+			c_convar* fp = g_weebware.g_convars->find_cvar("firstperson");
+			fp->SetValue("1");
+			done = false;
+		}
+	}
 }
