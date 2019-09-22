1f23b346271a4f01cb31a5fe0c4a6dc6f33671d3
Justin <justin@garofolo.net>
2019-09-21 23:52:49 -0400

    created new dependency downloader project

---
+++ weebware - dependency downloader/dependency downloader/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace dependency_downloader {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+        }
+
+    }
+
+}
