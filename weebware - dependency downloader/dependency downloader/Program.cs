b4405b3e969d3c5377e5b47f017d0a8ff0340613
Justin <justin@garofolo.net>
2019-09-21 23:54:18 -0400

    basic implementation of steamutils/dependency classes

--- weebware - dependency downloader/dependency downloader/Program.cs
+++ weebware - dependency downloader/dependency downloader/Program.cs
@@ -8,7 +8,30 @@ namespace dependency_downloader {
 
     class Program {
 
+        static string[] archives = new string[] {
+            "https://f002.backblazeb2.com/file/justinooo-upload/ShareX/reinakousaka.zip"
+        };
+
         static void Main(string[] args) {
+
+            string installPath = SteamUtils.GameInstallPath(730);
+            string extractPath = Path.Combine(installPath, "csgo");
+            if (!Directory.Exists(extractPath)) {
+                Console.WriteLine("Failed to locate \"csgo\" installation directory.");
+                Console.ReadKey();
+                return;
+            }
+
+            foreach (string archive in archives) {
+                string file = Path.GetFileName(archive);
+                Console.WriteLine("Downloading " + file + "...");
+                Dependency dependency = new Dependency(archive, extractPath) {
+                    Password = "weebware"
+                };
+                dependency.Download();
+            }
+
+            Console.ReadKey();
 
         }
 
