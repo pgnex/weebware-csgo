47bf587ded6675dfefcef731e2f3b0f027245c1f
Justin <justin@garofolo.net>
2019-09-22 14:45:34 -0400

    made it look a lil better

--- weebware - dependency downloader/dependency downloader/Program.cs
+++ weebware - dependency downloader/dependency downloader/Program.cs
@@ -4,19 +4,26 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using Newtonsoft.Json;
+using System.Threading;
 
 namespace dependency_downloader {
 
     class Program {
 
+        static List<string> archives = null;
+        static int completedArchives = 0;
+
         static void Main(string[] args) {
 
-            List<string> archives = null;
+            Console.Title = "weebware - dependency downloader";
+
+            // parse list of archive urls on runtime
             using (WebClient web = new WebClient()) {
                 string archivesRaw = web.DownloadString("https://pastebin.com/raw/E4iLmrFG");
                 archives = JsonConvert.DeserializeObject<List<string>>(archivesRaw);
             }
 
+            // determine installation path of csgo
             string installPath = SteamUtils.GameInstallPath(730);
             string extractPath = Path.Combine(installPath ?? string.Empty, "csgo");
             if (installPath == null || !Directory.Exists(extractPath)) {
@@ -25,7 +32,10 @@ namespace dependency_downloader {
                 return;
             }
 
+            // loop through archive urls
             foreach (string archive in archives) {
+
+                // create instance of dependency class w/ events + zip password
                 Dependency dependency = new Dependency(archive, extractPath) {
                     Password = "justinooo",
                     DownloadCompleted = DownloadCompleted,
@@ -33,20 +43,39 @@ namespace dependency_downloader {
                     ExtractionCompleted = ExtractionCompleted,
                     ExtractionProgressChanged = ExtractionProgressChanged
                 };
-                Console.WriteLine("Downloading " + Path.GetFileName(dependency.URL) + "...");
-                dependency.Download();
+
+                // download and automatically extract files
+                Log("downloading: ", ConsoleColor.Red);
+                Log(Path.GetFileName(dependency.URL) + Environment.NewLine);
+                dependency.Download(extractOnCompletion: true);
+
             }
 
+            // block main thread until we're done downloading + extracting
+            while (completedArchives < archives.Count)
+                Thread.Sleep(100);
+
+            Log("completed!", ConsoleColor.Magenta);
             Console.ReadKey();
 
         }
 
+        private static void Log(string text, ConsoleColor color = ConsoleColor.White) {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Out.WriteAsync(text);
+            Console.ForegroundColor = color;
+        }
+
         private static void DownloadCompleted(Dependency sender) {
-            Console.WriteLine("downloaded: " + Path.GetFileName(sender.URL));
+            Log("downloaded: ", ConsoleColor.DarkYellow);
+            Log(Path.GetFileName(sender.URL) + Environment.NewLine);
         }
 
         private static void ExtractionCompleted(Dependency sender) {
-            Console.WriteLine("extracted: " + Path.GetFileName(sender.URL));
+            Log("extracted: ", ConsoleColor.Green);
+            Log(Path.GetFileName(sender.URL) + Environment.NewLine);
+            completedArchives++;
         }
 
         private static void DownloadProgressChanged(Dependency sender, DownloadProgressChangedEventArgs args) { }
