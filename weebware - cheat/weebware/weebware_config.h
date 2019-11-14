aaf5c7be8bd09092ff1a9ab3d514b9b88c3af82b
nexzoz <nexzozgaming@gmail.com>
2019-11-13 20:24:23 -0800

    rage options added to menu & config file

--- weebware - cheat/weebware/weebware_config.h
+++ weebware - cheat/weebware/weebware_config.h
@@ -22,6 +22,10 @@ static bool check_color(std::string key, json data) {
 	return false;
 }
 
+struct t_config_info {
+	std::string title = "";
+};
+
 class c_config_list
 {
 public:
@@ -364,6 +368,19 @@ public:
 	int auto_defuse_key;
 	bool no_duck_cooldown;
 	std::string weapon_option_name = "Weapon Options - Hold Out A Weapon";
+
+	// ragebot
+	bool ragebot_enabled;
+	bool safemode = true;
+	bool autowall_enabled;
+	float autowall_min_dmg = 1.f;
+	bool ragebot_target_team;
+	bool autoshoot_enabled;
+	bool hitchance_enabled;
+	float ragebot_hitchance;
+	bool no_recoil;
+	bool ragebot_silent_aim;
+	bool ragebot_autostop;
 
 	// debug stuff
 	bool on_sendpacket = 0;
