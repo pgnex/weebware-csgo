a962220fcb03152e73b9841503f313a901727e5c
nexzoz <nexzozgaming@gmail.com>
2020-01-24 09:04:45 -0800

    load custom font and play welcome message from resources

--- weebware - loader 2.0/weebware loader 2.0/General/formstuff.cs
+++ weebware - loader 2.0/weebware loader 2.0/General/formstuff.cs
@@ -1,13 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace weebware_loader_2._0.General {
+
+namespace weebware_loader.General {
     class formstuff {
+
+        public static PrivateFontCollection createfont() {
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            using (Stream fontstream = Assembly.GetExecutingAssembly().GetManifestResourceStream("weebware_loader.Resources.CircularStd.ttf")) {
+                IntPtr data = Marshal.AllocCoTaskMem(Convert.ToInt32(fontstream.Length));
+                int bufsize = BitConverter.ToInt32(BitConverter.GetBytes(fontstream.Length), 0);
+                byte[] fontdata = new byte[bufsize];
+                fontstream.Read(fontdata, 0, Convert.ToInt32(fontstream.Length));
+                Marshal.Copy(fontdata, 0, data, Convert.ToInt32(fontstream.Length));
+                pfc.AddMemoryFont(data, Convert.ToInt32(fontstream.Length));
+                fontstream.Close();
+                Marshal.FreeCoTaskMem(data);
+            }
+
+            return pfc;
+        }
+
+        public static void play_welcome(CheckBox checkBox) {
+
+            try {
+                checkBox.Checked = Properties.Settings.Default.disablesounds;
+            } catch (Exception) {
+                Properties.Settings.Default.disablesounds = false;
+                Properties.Settings.Default.Save();
+            }
+
+            if (!Properties.Settings.Default.disablesounds) {
+                Stream str = Properties.Resources.welcome;
+                SoundPlayer snd = new SoundPlayer(str);
+                snd.Play();
+            }
+        }
 
 
         // this is for making the form components movable since we arent using an actual window
