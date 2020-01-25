2473c92cec471c85efd42d67dd6a446641fe802e
nexzoz <nexzozgaming@gmail.com>
2020-01-25 11:56:45 -0800

    settings and app manifest file

--- weebware - loader 2.0/weebware loader 2.0/Properties/Settings.Designer.cs
+++ weebware - loader 2.0/weebware loader 2.0/Properties/Settings.Designer.cs
@@ -45,5 +45,17 @@ namespace weebware_loader.Properties {
                 this["disablesounds"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string username {
+            get {
+                return ((string)(this["username"]));
+            }
+            set {
+                this["username"] = value;
+            }
+        }
     }
 }
