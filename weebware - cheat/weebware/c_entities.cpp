9006a6454b67f8f9515a37a8ca20c277b491a116
drtfghujk04 <nex@weebware.net>
2020-04-20 21:25:05 -0700

    set angles for fake angle chams

--- weebware - cheat/weebware/c_entities.cpp
+++ weebware - cheat/weebware/c_entities.cpp
@@ -6,4 +6,9 @@ c_basecombat_weapon * c_base_entity::m_pActiveWeapon()
 	return (c_basecombat_weapon*)(g_weebware.g_entlist->getcliententityfromhandle((HANDLE)*(PULONG)((DWORD)this + iOffset)));
 }
 
-
+void c_base_entity::set_angles(Vector angles) 
+{
+	typedef void(__thiscall* set_angle_fn)(void*, const Vector&);
+	static set_angle_fn set_angle = (set_angle_fn)g_weebware.pattern_scan("client_panorama.dll", "55 8B EC 83 E4 F8 83 EC 64 53 56 57 8B F1");
+	set_angle(this, angles);
+}
