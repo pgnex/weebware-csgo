75643ed9fc1f60ed0048e4790a591d819ef5e893
drtfghujk04 <nexzozgaming@gmail.com>
2020-04-21 22:30:42 -0700

    legit aa

--- weebware - cheat/weebware/legit_aa.cpp
+++ weebware - cheat/weebware/legit_aa.cpp
@@ -1,46 +1,51 @@
 #include "Header.h"
 #include "legit_aa.h"
 
-LegitAntiAim g_LegitAntiAim;
-
-float updateTime2;
-float lastUpdate2;
-float wasmoving2;
-bool performBreak2;
-bool m_bBreakBalance2 = false;
-
-float max_delta(c_base_entity* pEnt) {
-
-	auto animstate = uintptr_t(pEnt->AnimState());
-
-	float duckammount = *(float*)(animstate + 0xA4);
-	float speedfraction = max(0, min(*reinterpret_cast<float*>(animstate + 0xF8), 1));
-
-	float speedfactor = max(0, min(1, *reinterpret_cast<float*> (animstate + 0xFC)));
-
-	float unk1 = ((*reinterpret_cast<float*> (animstate + 0x11C) * -0.30000001f) - 0.19999999f)* speedfraction;
-	float unk2 = unk1 + 1.f;
-	float unk3;
-
-	if (duckammount > 0) {
-		unk2 += ((duckammount * speedfactor) * (0.5f - unk2));
-	}
-
-	unk3 = *(float*)(animstate + 0x334) * unk2;
-
-	return unk3;
-}
-
-
 void LegitAntiAim::Run(c_usercmd* cmd)
 {
-	auto local = g_weebware.g_entlist->getcliententity(g_weebware.g_engine->get_local());
+    c_base_entity* local = g_weebware.g_entlist->getcliententity(g_weebware.g_engine->get_local());
 
-	if (!local)
-		return;
+    if (!local)
+        return;
 
-	if (!g_weebwarecfg.misc_legit_aa_enabled)
-		return;
+    static bool fakelag_reset = false;
 
+    if (!g_weebwarecfg.misc_legit_aa_enabled) {
+        if (!fakelag_reset) {
+            g_weebwarecfg.fake_lag_factor = 0;
+            g_weebwarecfg.fake_lag = 0;
+            fakelag_reset = true;
+        }
+        return;
+    }
 
+    if (g_weebwarecfg.fake_lag_factor <= 2) g_weebwarecfg.fake_lag_factor = 2;
+    if (g_weebwarecfg.fake_lag_factor >= 6) g_weebwarecfg.fake_lag_factor = 6;
+    g_weebwarecfg.fake_lag = 2;
+    fakelag_reset = false;
+
+    c_basecombat_weapon* weapon = local->m_pActiveWeapon();
+
+    if (!weapon)
+        return;
+
+    if (weapon->is_grenade())
+        return;
+
+    if (cmd->buttons & in_attack) return;
+    if (cmd->buttons & in_use) return;
+
+    int local_move_type = local->get_move_type();
+    if (local_move_type == MOVETYPE_LADDER || local_move_type == MOVETYPE_NOCLIP) return;
+
+    float& ptr_sidemove = cmd->sidemove;
+
+    if (!(cmd->buttons & in_moveleft) && !(cmd->buttons & in_moveright) && !(cmd->buttons & in_forward) && !(cmd->buttons & in_back))
+        if (cmd->buttons & in_duck)
+            (cmd->command_number % 2) ? ptr_sidemove += -4.6 : ptr_sidemove += 4.6;
+        else
+            (cmd->command_number % 2) ? ptr_sidemove += -1.6 : ptr_sidemove += 1.6;
+
+    if (!g_weebware.send_packet)
+        cmd->viewangles.y += local->get_max_theoretical_desync() * (GetKeyState(g_weebwarecfg.misc_legit_aa_side_key) ? 1 : -1);
 }
