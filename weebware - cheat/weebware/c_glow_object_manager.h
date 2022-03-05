fd5f33d4b519d6d09fb85201c4c59913fe4ac78e
9xnxy <nex@weebware.net>
2022-03-04 17:29:25 -0800

    updated glowobject class - fixed glow

--- weebware - cheat/weebware/c_glow_object_manager.h
+++ weebware - cheat/weebware/c_glow_object_manager.h
@@ -7,35 +7,36 @@ class GlowObjectDefinition_t
 public:
 	GlowObjectDefinition_t() { memset(this, 0, sizeof(*this)); }
 
-	class c_base_entity* m_pEntity;    //0x0000
+
+	int32_t m_nNextFreeSlot;
+	class c_base_entity* m_pEntity;
 	union
 	{
-		Vector m_vGlowColor;           //0x0004
+		Vector m_vGlowColor;           
 		struct
 		{
-			float   m_flRed;           //0x0004
-			float   m_flGreen;         //0x0008
-			float   m_flBlue;          //0x000C
+			float   m_flRed;           
+			float   m_flGreen;         
+			float   m_flBlue;
+			float	m_flAlpha;
 		};
 	};
-	float   m_flAlpha;                 //0x0010
-	uint8_t pad_0014[4];               //0x0014
-	float   m_flSomeFloat;             //0x0018
-	uint8_t pad_001C[4];               //0x001C
-	float   m_flAnotherFloat;          //0x0020
-	bool    m_bRenderWhenOccluded;     //0x0024
-	bool    m_bRenderWhenUnoccluded;   //0x0025
-	bool    m_bFullBloomRender;        //0x0026
-	uint8_t pad_0027[5];               //0x0027
-	int32_t m_nGlowStyle;              //0x002C
-	int32_t m_nSplitScreenSlot;        //0x0030
-	int32_t m_nNextFreeSlot;           //0x0034
+	bool m_bGlowAlphaCappedByRenderAlpha;
+	float m_flGlowAlphaFunctionOfMaxVelocity;
+	float m_flGlowAlphaMax;
+	float m_flGlowPulseOverdrive;
+	bool m_bRenderWhenOccluded;
+	bool m_bRenderWhenUnoccluded;
+	bool m_bFullBloomRender;
+	int m_nFullBloomStencilTestValue;
+	int m_nGlowStyle;
+	int m_nSplitScreenSlot;
 
 	bool IsUnused() const { return m_nNextFreeSlot != GlowObjectDefinition_t::ENTRY_IN_USE; }
 
 	static const int END_OF_FREE_LIST = -1;
 	static const int ENTRY_IN_USE = -2;
-}; //Size: 0x0038 (56)
+};
 
 class CGlowObjectManager
 {
